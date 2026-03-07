e9e1b1f412bff903a441fa6ae1196ab2412d921d
vande <vanderploeg.matthew@gmail.com>
2026-03-07 10:04:30 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/FRC2026-ScoutingApp/sw.js', { scope: '/FRC2026-ScoutingApp/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}

b8b996b9769d5f43640d76eda180ba166f62b2fc
vande <vanderploeg.matthew@gmail.com>
2026-03-21 10:26:19 -0400

    fixed pit team number

--- src/components/pit/PitScoutForm.jsx
+++ src/components/pit/PitScoutForm.jsx
@@ -64,7 +64,7 @@ export function PitScoutForm({ teamNumber, onSave }) {
           placeholder="e.g., 107"
           required
           min="1"
-          max="9999"
+          max="99999"
         />
       </div>
 
